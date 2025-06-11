01888bae75b64b80d3e9db054a00789908c23ddc
Daniel Castillo <muzakryder2@msn.com>
2025-06-11 18:39:48 -0500

    Add DaisyUI theme, add HomePage UI and Navbar, add NoteCard

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,21 @@
 import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
 import noteRoutes from './routers/notesRouter.js'
 import { connectDB } from './config/db.js'
 import rateLimiter from './middleware/rateLimiter.js'
-import dotenv from 'dotenv'
+
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
 const app = express()
 
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  })
+)
 app.use(express.json())
 app.use(rateLimiter)
 
