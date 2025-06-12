14a4c11eb109798b1bb168f2cdbd8b64d1fe24ed
Daniel Castillo <muzakryder2@msn.com>
2025-06-11 23:49:04 -0500

    add delete note functionality

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import api from '../lib/axios'
 import Navbar from '../components/Navbar'
 import RateLimitedUI from '../components/RateLimitedUI'
 import NoteCard from '../components/NoteCard'
+import NotesNotFound from '../components/NotesNotFound'
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false)
@@ -40,10 +41,12 @@ const HomePage = () => {
           <div className='text-center text-primary py-10'>Loading notes...</div>
         )}
 
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+
         {notes.length > 0 && !isRateLimited && (
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
             {notes.map((note) => (
-              <NoteCard key={note._id} note={note} />
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
             ))}
           </div>
         )}
