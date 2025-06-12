1ed0801aa292804fe6156e5fee6e92d7dcad4592
Daniel Castillo <muzakryder2@msn.com>
2025-06-12 10:20:20 -0500

    prepare v1.0 for deplyment on render

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import path from 'path'
 
 import noteRoutes from './routers/notesRouter.js'
 import { connectDB } from './config/db.js'
@@ -8,18 +9,29 @@ import rateLimiter from './middleware/rateLimiter.js'
 
 dotenv.config()
 
-const PORT = process.env.PORT || 5000
 const app = express()
+const PORT = process.env.PORT || 5000
+const __dirname = path.resolve()
 
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-  })
-)
+if (process.env.NODE_ENV !== 'production') {
+  app.use(
+    cors({
+      origin: 'http://localhost:5173',
+    })
+  )
+}
+
 app.use(express.json())
 app.use(rateLimiter)
 
 app.use('/api/notes', noteRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '..', '/frontend', 'dist')))
+  app.get('/{*any}', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'))
+  })
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
